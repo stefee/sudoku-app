2e4f244764d3c3f8a05f2ae53ffa49909504008a
Stef <stef.jones@condenast.com>
2020-07-04 11:53:12 +0100

    feat: add SudokuTable component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,55 @@
 import Head from 'next/head'
 
+const SudokuCell = ({ id, label, digit = null }) => {
+  const cellId = `sudoku-cell-${id}`;
+
+  return (
+    <td>
+      <label for={cellId}>
+        {label}
+      </label>
+      <input
+        id={cellId}
+        name={cellId}
+        type="number"
+        value={digit}
+        max={9}
+        min={1}
+      />
+    </td>
+  );
+};
+
+const SudokuTableRow = ({ rowNumber }) => {
+  const cells = [];
+
+  for (const columnNumber of [1, 2, 3, 4, 5, 6, 7, 8, 9]) {
+    const cellId = `r${rowNumber}c${columnNumber}`;
+    const cellLabel = cellId.toUpperCase();
+
+    cells.push(
+      <SudokuCell
+        id={cellId}
+        label={cellLabel}
+      />
+    )
+  }
+
+  return <tr>{cells}</tr>;
+};
+
+const SudokuTable = () => {
+  const tableRows = [];
+
+  for (const rowNumber of [1, 2, 3, 4, 5, 6, 7, 8, 9]) {
+    tableRows.push(
+      <SudokuTableRow rowNumber={rowNumber} />
+    )
+  }
+
+  return <table>{tableRows}</table>;
+};
+
 export default function Home() {
   return (
     <div className="container">
@@ -8,9 +58,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>
-          Welcome to Sudoku App
-        </h1>
+        <SudokuTable />
       </main>
 
       <style jsx global>{`
@@ -25,6 +73,49 @@ export default function Home() {
 
         * {
           box-sizing: border-box;
+        }
+
+        table {
+          border-collapse: collapse;
+          margin: 10px auto;
+          font-size: 2rem;
+        }
+
+        td {
+          border: 1px solid black;
+          width: 1.6em;
+          height: 1.6em;
+          overflow: hidden;
+          text-align: center;
+        }
+
+        td:first-child {
+          border-left: 3px solid black;
+        }
+
+        td:nth-child(3n) {
+          border-right: 3px solid black;
+        }
+
+        tr:first-child {
+          border-top: 3px solid black;
+        }
+
+        tr:nth-child(3n) {
+          border-bottom: 3px solid black;
+        }
+
+        td label {
+          position: absolute;
+
+          width: 1px;
+          height: 1px;
+          margin: -1px;
+          border: 0;
+          padding: 0;
+
+          clip: rect(0 0 0 0);
+          overflow: hidden;
         }
       `}</style>
     </div>
